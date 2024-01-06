464383c67d4fc7109bb770c10fd6a1646fc6552d
Wyeben <yila.ben@yahoo.com>
2024-01-06 03:27:54 +0100

    added code

--- script/script.js
+++ script/script.js
@@ -1,20 +1,54 @@
-function setClock() {
-    const deg = 6;
-    const hourHand = document.querySelector('.hour-hand');
-    const minuteHand = document.querySelector('.minute-hand');
-    const secondHand = document.querySelector('.second-hand');
-  
-    setInterval(() => {
-      const date = new Date();
-      const hour = date.getHours() * 30;
-      const minute = date.getMinutes() * deg;
-      const second = date.getSeconds() * deg;
-  
-      hourHand.style.transform = `rotate(${hour + minute / 12}deg)`;
-      minuteHand.style.transform = `rotate(${minute}deg)`;
-      secondHand.style.transform = `rotate(${second}deg)`;
-    }, 1000);
+const viewTimeButton = document.querySelector('.view-time-btn');
+const viewDateButton = document.querySelector('.view-date-btn');
+const timerButton = document.querySelector('.timer-btn');
+const hourHand = document.querySelector('.hour-hand');
+const minuteHand = document.querySelector('.minute-hand');
+const secondHand = document.querySelector('.second-hand');
+const view = document.querySelector('.view');
+
+let timeIntervalId;
+
+viewTimeButton.addEventListener('click', () => {
+  if (!timeIntervalId) {
+    updateTimeInView();
+    timeIntervalId = setInterval(updateTimeInView, 1000); 
   }
-  
-  setClock();
-  
+});
+
+viewDateButton.addEventListener('click', () => {
+  clearInterval(timeIntervalId); 
+  timeIntervalId = null; 
+  const currentDate = new Date();
+  const day = currentDate.getDate().toString().padStart(2, '0');
+  const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+  const year = currentDate.getFullYear();
+  view.innerHTML = `Date: ${year}-${month}-${day}`;
+});
+
+timerButton.addEventListener('click', () => {
+  document.location.href = 'timer/index.html'
+})
+
+function updateTimeInView() {
+  const currentTime = new Date();
+  const hour = currentTime.getHours().toString().padStart(2, '0');
+  const minute = currentTime.getMinutes().toString().padStart(2, '0');
+  const second = currentTime.getSeconds().toString().padStart(2, '0');
+  view.innerHTML = `Time: ${hour}:${minute}:${second}`;
+}
+
+function setClock() {
+  const deg = 6;
+  setInterval(() => {
+    const date = new Date();
+    const hour = date.getHours() * 30;
+    const minute = date.getMinutes() * deg;
+    const second = date.getSeconds() * deg;
+
+    hourHand.style.transform = `rotate(${hour + minute / 12}deg)`;
+    minuteHand.style.transform = `rotate(${minute}deg)`;
+    secondHand.style.transform = `rotate(${second}deg)`;
+  }, 1000);
+}
+
+setClock();
---
+++ timer/script.js
@@ -0,0 +1,64 @@
+let timerHse = document.querySelector('.timer-hse');
+let playButton = document.querySelector('.play-btn');
+let resetButton = document.querySelector('.reset-btn');
+let clockButton = document.querySelector('.clock-btn');
+
+
+
+let hours = 0;
+let minute = 0;
+let seconds = 0;
+
+let intervalId = null;
+
+playButton.addEventListener('click', () => {
+
+  if(intervalId === null){
+    intervalId = setInterval(play, 1000);
+    playButton.innerHTML = 'Pause';
+    playButton.classList.remove('play-btn');
+    playButton.classList.add('pause-btn');
+  }else{
+    clearInterval(intervalId);
+    intervalId = null;
+    playButton.innerHTML = 'Play';
+    playButton.classList.remove('pause-btn');
+    playButton.classList.add('play-btn');
+
+  }
+
+});
+
+resetButton.addEventListener('click', () => {
+    clearInterval(intervalId);
+    intervalId = null;
+    hours = 0;
+    minute = 0;
+    seconds = 0;
+    displayTime();
+    playButton.innerHTML = 'Play';
+    playButton.classList.remove('pause-btn');
+    playButton.classList.add('play-btn');
+});
+
+clockButton.addEventListener('click', () => {
+    window.location.href = '../index.html'
+});
+
+function play(){
+    seconds++;
+    if(seconds === 60){
+        seconds = 0;
+        minute++;
+
+        if(minute === 60){
+            minute = 0;
+            hours++;
+        }
+    }
+    displayTime();
+}
+
+function displayTime(){
+    timerHse.innerHTML = `${hours.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+}
